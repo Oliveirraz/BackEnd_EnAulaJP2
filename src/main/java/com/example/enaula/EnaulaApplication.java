15cb3e7b513b120b871d8a0095650820eb4e2548
celio andrade <celio.roberto@academico.ifpb.edu.br>
2026-08-31 18:29:17 -0300

    comentario na aplicação

--- src/main/java/com/example/enaula/EnaulaApplication.java
+++ src/main/java/com/example/enaula/EnaulaApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EnaulaApplication {
+public class EnaulaApplication {// aplicação
 
 	public static void main(String[] args) {
 		SpringApplication.run(EnaulaApplication.class, args);
