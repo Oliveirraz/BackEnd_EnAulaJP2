7e297ba9df11ef2bb0c580e02a734034a0fed7a8
thays silva <silva.thays@academico.ifpb.edu.br>
2026-09-19 16:38:00 -0300

    feat/criando classe aula e enum modalidade,e ajustando professor

--- src/main/java/com/example/enaula/entity/Aula.java
+++ src/main/java/com/example/enaula/entity/Aula.java
@@ -1,4 +1,40 @@
 package com.example.enaula.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Entity
+@Table(name = "aula")
+@Getter
+@Setter
 public class Aula {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "materia_id", nullable = false)
+    private Materia materia;
+
+    @ManyToOne
+    @JoinColumn(name = "professor_id", nullable = false)
+    private Professor professor;
+
+    @Column(nullable = false)
+    private LocalDate data;
+
+    @Column(nullable = false)
+    private LocalTime horario;
+
+    @Column(nullable = false)
+    private Integer duracao;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Modalidade modalidade;
 }
